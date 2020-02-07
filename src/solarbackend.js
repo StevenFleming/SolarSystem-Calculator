d1d621d2c6e07b6ecd39906f5e719098868a5d45
Student <example@example.com>
2020-02-07 15:05:52 -0800

    corrects typos

--- src/solarbackend.js
+++ src/solarbackend.js
@@ -30,32 +30,14 @@ export class PlanetCalculator{
         let jupiterAge =this.age/(11.86);
         let jupiterYearsLeft =lifeExpectencyJupiter - jupiterAge
         let textYearsLeft = `You have about ${jupiterYearsLeft.toPrecision(3)} left to live`;
-            if (jupiterYearsLeft>0){
+            if (jupiterYearsLeft>=0){
                 return textYearsLeft
-            }else{
+            } else{
                 let jupiterYearsLeft =lifeExpectencyJupiter - jupiterAge
                 let jupiterYearsLeftAbsoluteValue = jupiterYearsLeft* (-1)
-                let textYearsPast =` You have surpassed Jupiters life expetency by ${jupiterYearsLeftAbsoluteValue.toPrecision(3)}` 
+                let textYearsPast =` You have surpassed Jupiters life expectency by ${jupiterYearsLeftAbsoluteValue.toPrecision(3)}` 
                 return textYearsPast
             }
-        // return jupiterAge.toPrecision(2);
         }
 
     }
-
-
-
-
-// // Constructor for User
-//    function User (age, planet){
-
-//     this.age = age;
-//     this.planet= planet;
-//     this.planetcount=0;
-//     }
-
-//     Mercury() {
-//         var marsage = this.age/(.24);
-//         return marsage;
-//     }
-// }
