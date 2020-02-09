4651d19e8c546570c31f73813ca4b0aca2ff2d2b
Student <example@example.com>
2020-02-08 18:26:34 -0800

    refractors code and adds in ability to calculate time to each planet in earth years and planet years

--- __tests__/example.test.js
+++ __tests__/example.test.js
@@ -4,22 +4,22 @@ import { exportAllDeclaration } from '@babel/types';
 describe ("Planets", () => {
     test ('Should calculate Earth Years to Mercury Years', () => {
         let userinput = new PlanetCalculator(1)
-        expect (userinput.Mercury()).toEqual("4.2");
+        expect (userinput.mercuryAge.toPrecision(2)).toEqual("4.2");
     })
 
     test ('Should calculate Earth Years into Venus Years', () => {
         let userinput = new PlanetCalculator(1)
-        expect (userinput.Venus()).toEqual("1.6");
+        expect (userinput.venusAge.toPrecision(2)).toEqual("1.6");
     })
 
     test ('Should calculate Earth Years into Mars Years', () => {
         let userinput = new PlanetCalculator(1)
-        expect (userinput.Mars()).toEqual("0.53");
+        expect (userinput.marsAge.toPrecision(2)).toEqual("0.53");
     })
 
     test ('Should calculate Earth Years into Jupiter Years', () => {
         let userinput = new PlanetCalculator(1)
-        expect (userinput.Jupiter()).toEqual("0.084");
+        expect (userinput.jupiterAge.toPrecision(2)).toEqual("0.084");
     })
 })
 
@@ -29,23 +29,34 @@ describe ("Planets", () => {
             expect (userinput.mercuryLifeExpectency()).toEqual("You have about 217 years left to live")
         })
 
-        test ('should return text string telling user how many jupiter years past life expectency they have lived', () => {
-            let userinput = new PlanetCalculator(73)
-            expect (userinput.jupiterLifeExpectency()).toEqual(` You have surpassed Jupiters life expectency by 0.0843 years`)
+        test ('should return text telling user they have x number of jupiter years left to live', () =>{
+            let user = new PlanetCalculator(1)
+            expect (user.jupiterLifeLeft()).toEqual("You have about 5.99 Jupiter years left to live")
         })
 
         test ('should return text string telling user how many mars years past life expectency they have lived', () => {
             let userinput = new PlanetCalculator(71)
-            expect (userinput.marsLifeExpectency()).toEqual("You have about 0.532 years left to live")
+            expect (userinput.marsLifeExpectency()).toEqual("You have about 0.532 mars years left to live")
         })
 
         test ('should return text string telling user how many venus years past life expectency they have lived', () => {
-            let userinput = new PlanetCalculator(71)
-            expect (userinput.venusLifeExpectency()).toEqual( `You have about 1.61 years left to live`)
+            let userinput = new PlanetCalculator(73)
+            expect (userinput.venusLifeExpectency()).toEqual( " You have surpassed venus life expectency by 1.61 years")
         })
 
     })
 
+    describe ("Years to Mars", () => {
+        test ( "should tell use how many earth years and mars years to travel to mars", () => {
+            let userinput =new PlanetCalculator(1)
+            expect (userinput.yearsToMars()).toEqual("traveling in a straight line under perfect conditions it would take approximately 1.56 years to travel to mars. Or 0.8297872340425533 mars years ")
+        })
+
+        test ( "should tell use how many earth years and mars years to travel to Jupiter", () => {
+            let userinput =new PlanetCalculator(1)
+            expect (userinput.yearsToJupiter()).toEqual( "traveling in a straight line under perfect conditions it would take approximately 17.96408674285714 years to travel to Jupiter. Or 1.514678477475307 Jupiter years ")
+        })
+    })
 
 
 
--- src/solarbackend.js
+++ src/solarbackend.js
@@ -1,32 +1,60 @@
 export class PlanetCalculator{
 
     constructor (age){
-
+    this.name = name
     this.age = age;
     this.lifeExpectency=72;
+    this.velocity = 35000000; //fastest speed ever reached by humans extrapolated to Km per year
+
+    this.mercuryAge= this.age/(.24);
+    this.mercuryLifelifeExpectency = this.lifeExpectency/(.24)
+    this.mercurydistance= 48000000; //shortest distance in km
+    
+    this.venusAge = this.age/(.62);
+    this.venusLifelifeExpectency = this.lifeExpectency/(.62);
+    this.venusDistance =38000000; //shortest distance in km
+
+    this.marsAge =this.age/(1.88);
+    this.marsLifelifeExpectency =this.lifeExpectency/(1.88);
+    this.marsDistance =54600000; //shortest distance in km
+          
+    this.jupiterAge =this.age/(11.86);
+    this.jupiterLifeExpectency = this.lifeExpectency/(11.86);
+    this.jupiterDistance= 628743036; //shortest distance in km                                               
     }
 
-    Mercury() {
-        let mercuryAge = this.age/(.24);
-        return mercuryAge.toPrecision(2);
+    yearsToMars() {
+        let yearsToMars = (this.marsDistance /this.velocity);
+        let marsYearsToMars = yearsToMars/(1.88);
+        let textYearsTo = `traveling in a straight line under perfect conditions it would take approximately ${yearsToMars} years to travel to mars. Or ${marsYearsToMars} mars years `
+        return textYearsTo
     }
-    Venus() {
-        let venusAge = this.age/(.62);
-        return venusAge.toPrecision(2);
+
+    yearsToMercury() {
+        let yearsToMercury = (this.mercuryDistance /this.velocity);
+        let mercuryYearsToMercury = yearsToMercury/(.24);
+        let textYearsTo = `traveling in a straight line under perfect conditions it would take approximately ${yearsToMercury} years to travel to Mercury. Or ${mercuryYearsToMercury} Mercury years `
+        return textYearsTo
     }
-    Mars() {
-        let marsAge =this.age/(1.88);
-        return marsAge.toPrecision(2);
+
+
+    yearsToJupiter() {
+        let yearsToJupiter = (this.jupiterDistance /this.velocity);
+        let jupiterYearsToJupiter = yearsToJupiter/(11.86);
+        let textYearsTo = `traveling in a straight line under perfect conditions it would take approximately ${yearsToJupiter} years to travel to Jupiter. Or ${jupiterYearsToJupiter} Jupiter years `
+        return textYearsTo
     }
-    Jupiter() {
-        let jupiterAge =this.age/(11.86);
-        return jupiterAge.toPrecision(2);
+
+    yearsToVenus() {
+        let yearsToVenus = (this.venusDistance /this.velocity);
+        let venusYearsToVenus = yearsToVenus/(.62);
+        let textYearsTo = `traveling in a straight line under perfect conditions it would take approximately ${yearsToVenus} years to travel to Venus. Or ${venusYearsToVenus} Venus years `
+        return textYearsTo
     }
+
 
     mercuryLifeExpectency() {
-        const lifeExpectencymercury = this.lifeExpectency/(.24);
-        let mercuryAge =this.age/(.24);
-        let mercuryYearsLeft =lifeExpectencymercury - mercuryAge
+        let mercuryYearsLeft = this.mercuryLifelifeExpectency - this.mercuryAge
         let textYearsLeft = `You have about ${mercuryYearsLeft.toPrecision(3)} years left to live`;
             if (mercuryYearsLeft>=0){
                 return textYearsLeft
@@ -37,11 +65,9 @@ export class PlanetCalculator{
         }
     }
     venusLifeExpectency() {
-        const lifeExpectencyvenus = this.lifeExpectency/(.62);
-        let venusAge =this.age/(.62);
-        let venusYearsLeft =lifeExpectencyvenus - venusAge;
-        let textYearsLeft = `You have about ${venusYearsLeft.toPrecision(3)} years left to live`;
+        let venusYearsLeft = this.venusLifelifeExpectency - this.venusAge;
             if (venusYearsLeft>=0){
+                let textYearsLeft = `You have about ${venusYearsLeft.toPrecision(3)} years left to live`;
                 return textYearsLeft
             } else if (venusYearsLeft<0){
                 let venusYearsLeftAbsoluteValue = venusYearsLeft* (-1)
@@ -49,29 +75,26 @@ export class PlanetCalculator{
                 return textYearsPast
         }
     }
-    marsLifeExpectency() {
-        const lifeExpectencymars = this.lifeExpectency/(1.88);
-        let marsAge =this.age/(1.88);
-        let marsYearsLeft =lifeExpectencymars - marsAge;
-        let textYearsLeft = `You have about ${marsYearsLeft.toPrecision(3)} years left to live`;
-            if (marsYearsLeft>=0){
-                return textYearsLeft
-            } else{                                               
-                let marsYearsLeftAbsoluteValue = marsYearsLeft* (-1)
-                let textYearsPast =` You have surpassed mars life expectency by ${marsYearsLeftAbsoluteValue.toPrecision(3)} years` 
-                return textYearsPast
-        }
-    }
-    jupiterLifeExpectency() {
-        const lifeExpectencyJupiter = this.lifeExpectency/(11.86);
-        let jupiterAge =this.age/(11.86);
-        let jupiterYearsLeft =lifeExpectencyJupiter - jupiterAge
-        let textYearsLeft = `You have about ${jupiterYearsLeft.toPrecision(3)} years left to live`;
+    jupiterLifeLeft() {
+        let jupiterYearsLeft =this.jupiterLifeExpectency - this.jupiterAge;
             if (jupiterYearsLeft>=0){
+                let textYearsLeft = `You have about ${jupiterYearsLeft.toPrecision(3)} Jupiter years left to live`;
                 return textYearsLeft
             } else{
                 let jupiterYearsLeftAbsoluteValue = jupiterYearsLeft* (-1)
                 let textYearsPast =` You have surpassed Jupiters life expectency by ${jupiterYearsLeftAbsoluteValue.toPrecision(3)} years` 
+                return textYearsPast
+        }
+    }
+
+    marsLifeExpectency() {
+        let marsYearsLeft =this.marsLifelifeExpectency - this.marsAge;
+            if (marsYearsLeft>=0){
+                let textYearsLeft = `You have about ${marsYearsLeft.toPrecision(3)} mars years left to live`;
+                return textYearsLeft
+            } else{                                               
+                let marsYearsLeftAbsoluteValue = marsYearsLeft* (-1)
+                let textYearsPast =` You have surpassed mars life expectency by ${marsYearsLeftAbsoluteValue.toPrecision(3)} mars years` 
                 return textYearsPast
         }
     }
